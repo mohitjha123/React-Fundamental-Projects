74955199dcc756db0d9c3704a0a83c1f2b4c1999
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-06-14 17:44:44 +0530

    14th-jun 6th-project

--- 06-tabs/starter/src/App.jsx
+++ 06-tabs/starter/src/App.jsx
@@ -1,6 +1,42 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useState, useEffect } from "react";
+import BtnContainer from "./BtnContainer";
+import JobInfo from "./JobInfo";
 
-const App = () => {
-  return <h2>Tabs Starter</h2>;
-};
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false); // we have to wright false because it will not load again after fetching data from url
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  if (loading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+  return (
+    <section className="jobs-center">
+      {/* btn container */}
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      {/* job info */}
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+}
+
 export default App;
